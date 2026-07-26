715fe498af62fecf662f86dd6e25bd465139b58e
Yasser Gandhi Hernández Esquivel <yasser.gandhi@gmail.com>
2026-07-25 22:35:10 -0600

    fix: actualiza señales de indexación para es-MX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
 // @ts-check
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import {
+  LAST_SIGNIFICANT_UPDATE,
+  SITE_URL,
+} from './src/config/siteMetadata.mjs';
 
-export default defineConfig({});
+export default defineConfig({
+  site: SITE_URL,
+  integrations: [
+    sitemap({
+      filter: (page) => new URL(page).pathname === '/',
+      serialize: (item) => ({
+        url: item.url,
+        lastmod: LAST_SIGNIFICANT_UPDATE,
+      }),
+    }),
+  ],
+});
